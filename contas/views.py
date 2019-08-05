f9b9a9100f5048d5f5ce17f92949d6ddc7a29a4d
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2019-08-05 15:16:10 -0300

    implementando crud

--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
+from .form import TransacaoForm
 import datetime
 
 # Create your views here.
@@ -12,3 +13,33 @@ def listagem(request):
     contexto = {}
     contexto['transacoes'] = Transacao.objects.all()
     return render(request, 'contas/listagem.html',contexto)
+
+def nova_transacao(request):
+    contexto = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    contexto['form'] = form 
+    return render(request, 'contas/form.html', contexto)
+
+def update(request, pk):
+    contexto = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    
+    contexto['form'] = form
+    contexto['transacao'] = transacao
+    return render(request, 'contas/form.html', contexto)
+
+def delete(request, pk):
+    contexto = {}
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
